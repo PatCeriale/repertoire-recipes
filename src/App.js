279fbe690e0b044dcbbdf8b61acab6a5b7c34843
Patrick Ceriale <patceriale@gmail.com>
2021-01-14 12:04:41 -0800

    started adding pages and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 // import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        {/* <Navbar /> */}
+        <Switch>
+          <Route exact path={"/"}>
+            <Home />
+          </Route>
+          {/* <Route exact path="/search">
+            <Search />
+          </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1>Hello World from Home!</h1>
+    </div>
+  );
+}
