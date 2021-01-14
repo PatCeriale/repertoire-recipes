4c205a02ee8edbb971c87d292ed12b5aed4f214e
Patrick Ceriale <patceriale@gmail.com>
2021-01-14 12:06:35 -0800

    added recipe list to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from "./logo.svg";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import RecipeList from "./pages/RecipeList/RecipeList";
 
 function App() {
   return (
@@ -11,9 +12,9 @@ function App() {
           <Route exact path={"/"}>
             <Home />
           </Route>
-          {/* <Route exact path="/search">
-            <Search />
-          </Route> */}
+          <Route exact path="/recipelist">
+            <RecipeList />
+          </Route>
         </Switch>
       </Router>
     </div>
---
+++ src/pages/RecipeList/RecipeList.js
@@ -0,0 +1,9 @@
+import "./RecipeList.css";
+
+export default function RecipeList() {
+  return (
+    <div className="Home">
+      <h1>Hello World from the Recipe List!</h1>
+    </div>
+  );
+}
