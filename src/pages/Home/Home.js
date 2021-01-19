a1665d8af242fbdca9988d835e5781890b13933c
Patrick Ceriale <patceriale@gmail.com>
2021-01-19 11:06:13 -0800

    styled navbar and added text to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <div className="Home">
       <h1>Hello World from Home!</h1>
+      <p>
+        Have you recently found a new tasty recipe and want to save it for
+        later? Add it to your Repertoire and cook up a meal momma would be proud
+        of!
+      </p>
     </div>
   );
 }
