f690a62239afa80500f1671c9c22b03850eb3719
Patrick Ceriale <patceriale@gmail.com>
2021-01-21 15:47:12 -0800

    added card styling to new recipe page

--- src/pages/NewRecipe/NewRecipe.js
+++ src/pages/NewRecipe/NewRecipe.js
@@ -3,7 +3,9 @@ import "./NewRecipe.css";
 export default function NewRecipe() {
   return (
     <div className="NewRecipe">
-      <h1>Hello World from the new recipe page!!</h1>
+      <div className="card" style={{ width: "90vw" }}>
+        <h1>Hello World from the new recipe page!!</h1>
+      </div>
     </div>
   );
 }
