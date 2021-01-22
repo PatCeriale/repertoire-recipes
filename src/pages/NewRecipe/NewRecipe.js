2296831cfcfa81c0d018f40fa48bdc51ad393439
Patrick Ceriale <patceriale@gmail.com>
2021-01-22 11:38:03 -0800

    fixed header on new recipes

--- src/pages/NewRecipe/NewRecipe.js
+++ src/pages/NewRecipe/NewRecipe.js
@@ -5,7 +5,7 @@ export default function NewRecipe() {
   return (
     <div className="NewRecipe">
       <div className="card" style={{ width: "90vw" }}>
-        <h1>Hello World from the new recipe page!!</h1>
+        <h1>Add a recipe</h1>
         <hr />
         <Form>
           <Form.Group controlId="formBasicTitle">
