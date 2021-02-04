2c8fc25208be9251b5c89c9b9c36115c3d88d55a
Patrick Ceriale <patceriale@gmail.com>
2021-02-04 12:47:30 -0800

    need to add route to db when it is online

--- src/components/ImageUpload/ImageUpload.js
+++ src/components/ImageUpload/ImageUpload.js
@@ -13,6 +13,7 @@ class ImageUpload extends React.Component {
       (error, result) => {
         if (!error && result && result.event === "success") {
           console.log(result.info.url);
+          // Return url to server when it is running correctly
         }
       }
     );
