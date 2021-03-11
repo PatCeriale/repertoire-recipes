6dc8cef1bc9c07e0f428886055ee67fbccfaa474
Patrick Ceriale <patceriale@gmail.com>
2021-03-10 17:53:27 -0800

    fixed update route typo

--- src/utils/API.js
+++ src/utils/API.js
@@ -13,12 +13,12 @@ export const getOneRecipe = function (userData) {
   return axios.get(`${baseURL}api/recipes/:id`, userData);
 };
 
-export const addNewRecipe = function (userData) {
+export const createRecipe = function (userData) {
   return axios.post(`${baseURL}api/newrecipe`, userData);
 };
 
 export const updateRecipe = function (userData) {
-  return axios.put(`${baseURL}updaterecipe/:id`, userData);
+  return axios.put(`${baseURL}api/updaterecipe/:id`, userData);
 };
 
 export const deleteRecipe = function (userData) {
